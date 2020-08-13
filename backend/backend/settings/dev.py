fb5ca31fbd00ee0f1b9fedfb56950d0685ec9da6
somefood <hsj4665@gmail.com>
2020-08-13 16:37:17 +0900

    01 프로젝트 생성 및 기본 환경 설정

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,13 @@
+from .common import *
+
+INSTALLED_APPS += [
+    'debug_toolbar'
+]
+
+
+# 맨 처음에 넣어야 하기 때문에 아래와 같이 둠
+MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
+] + MIDDLEWARE
+
+INTERNAL_IPS = ["127.0.0.1"]
