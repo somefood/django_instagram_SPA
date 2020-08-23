410017e0e4a9cc85ce05c3aa6b6647cdcb04bb0e
somefood <hsj4665@gmail.com>
2020-08-23 18:43:03 +0900

    12 포스팅 목록 API에 JWT 인증하기

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Axios from "axios";
 import Post from "./Post";
 import { useAppContext } from "store";
+import { Alert } from "antd";
 
 const apiUrl = "http://localhost:8000/api/posts/";
 
@@ -12,7 +13,8 @@ function PostList() {
   } = useAppContext();
   const [postList, setPostList] = useState([]);
   useEffect(() => {
-    Axios.get(apiUrl)
+    const headers = { Authorization: `JWT ${jwtToken}` };
+    Axios.get(apiUrl, { headers })
       .then((response) => {
         const { data } = response;
         console.log("loaded response:", response);
@@ -25,6 +27,7 @@ function PostList() {
   }, []);
   return (
     <div>
+      {postList.length === 0 && <Alert type="warning" message="포스트가 없습니다."/>}
       {postList.map((post) => (
         <Post post={post} key={post.id} />
       ))}
