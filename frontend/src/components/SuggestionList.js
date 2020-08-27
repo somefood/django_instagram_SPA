9df830909374909772d4481587a1e22e3c5f6a0f
somefood <iamseokjuhong@gmail.com>
2020-08-27 22:56:38 +0900

    15 유저 Follow 리액트 화면 구현

--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { Card } from "antd";
 import Suggestion from "./Suggestion";
 import useAxios from "axios-hooks"; // yarn add axios-hooks
@@ -12,24 +12,40 @@ const SuggestionList = ({ style }) => {
 
   const headers = { Authorization: `JWT ${jwtToken}` };
 
-  const [{ data: userList, loading, error }, refetch] = useAxios({
+  const [userList, setUserList] = useState([]);
+
+  const [{ data: origUserList, loading, error }, refetch] = useAxios({
     url: "http://localhost:8000/accounts/suggestions/",
     headers,
   });
+
+  useEffect(() => {
+    if (!origUserList) setUserList([]);
+    else
+      setUserList(origUserList.map((user) => ({ ...user, is_follow: false })));
+  }, [origUserList]);
+
+  const onFollowUser = (username) => {
+    setUserList((prevUserList) =>
+      prevUserList.map((user) =>
+        user.username !== username ? user : { ...user, is_follow: true }
+      )
+    );
+  };
 
   return (
     <div style={style}>
       {loading && <div>Loading...</div>}
       {error && <div>로딩 중에 에러가 발생했습니다.</div>}
-      <button onClick={()=> refetch()}>Reload</button>
+      <button onClick={() => refetch()}>Reload</button>
       <Card title="Suggestions for you" size="small">
-        {userList &&
-          userList.map((suggestionUser) => (
-            <Suggestion
-              key={suggestionUser.username}
-              suggestionUser={suggestionUser}
-            />
-          ))}
+        {userList.map((suggestionUser) => (
+          <Suggestion
+            key={suggestionUser.username}
+            suggestionUser={suggestionUser}
+            onFollowUser={onFollowUser}
+          />
+        ))}
       </Card>
     </div>
   );
