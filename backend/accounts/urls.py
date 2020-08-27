20e64e9f1c4543b0ba84ca74af3dd6d833438303
somefood <iamseokjuhong@gmail.com>
2020-08-27 23:41:16 +0900

    16 유저 Follow API 구현

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
-from .views import SignupView, SuggestionListAPIView
+from . import views
 
 urlpatterns = [
-    path('signup/', SignupView.as_view(), name='login'),
+    path('signup/', views.SignupView.as_view(), name='login'),
     path('token/', obtain_jwt_token),
     path('token/refresh/', refresh_jwt_token),
     path('token/verify/', verify_jwt_token),
 
-    path("suggestions/", SuggestionListAPIView.as_view(), name="suggestion_user_list"),
+    path("suggestions/", views.SuggestionListAPIView.as_view(), name="suggestion_user_list"),
+    path("follow/", views.user_follow, name="user_follow"),
+    path("unfollow/", views.user_unfollow, name="user_unfollow"),
 ]
--- frontend/src/components/SuggestionList.js
+++ frontend/src/components/SuggestionList.js
@@ -1,6 +1,7 @@
 import React, { useMemo, useState, useEffect } from "react";
 import { Card } from "antd";
 import Suggestion from "./Suggestion";
+import Axios from "axios";
 import useAxios from "axios-hooks"; // yarn add axios-hooks
 import { useAppContext } from "store";
 import "./SuggestionList.scss";
@@ -26,11 +27,19 @@ const SuggestionList = ({ style }) => {
   }, [origUserList]);
 
   const onFollowUser = (username) => {
-    setUserList((prevUserList) =>
-      prevUserList.map((user) =>
-        user.username !== username ? user : { ...user, is_follow: true }
-      )
-    );
+    const data = { username };
+    const config = { headers };
+    Axios.post("http://localhost:8000/accounts/follow/", data, config)
+      .then((response) => {
+        setUserList((prevUserList) =>
+          prevUserList.map((user) =>
+            user.username !== username ? user : { ...user, is_follow: true }
+          )
+        );
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   return (
